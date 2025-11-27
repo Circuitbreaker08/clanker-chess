91b37f4ece0a366b0e4cc70db9f136d1abb3b1a2
Circuitbreaker08 <circuitbreaker08@gmail.com>
2025-11-27 17:30:07 -0500

    I dont even know bro

--- src/main.cpp
+++ src/main.cpp
@@ -9,6 +9,7 @@
 // Otherwise could interfere with proper static serving and other functions
 
 int main() {
+    // Set cwd to file location
     std::filesystem::current_path(std::filesystem::canonical("/proc/self/exe").parent_path());
 
     crow::SimpleApp app;
@@ -18,12 +19,14 @@ int main() {
         res.end();
     });
 
-    //CROW_ROUTE(app, "/")
-    //([](const crow::request&, crow::response& res) {
-    //    //replace cat.jpg with your file path
-    //    res.set_static_file_info("cat.jpg");
-    //    res.end();
-    //});
+    CROW_ROUTE(app, "/api/token_validator").methods("POST"_method)
+    ([](const crow::request&, crow::response& res) {
+        //replace cat.jpg with your file path
+        res.set_static_file_info("cat.jpg");
+        res.end();
+
+        // TODO - check if token valid and add a call to the homepage
+    });
 
     app.port(8765).multithreaded().run();
 }
